df48c760aa6accc2f33ca594034a47afbaaa50a1
Anton Karmanov <a.karmanov@inventati.org>
2022-07-08 21:07:39 +0300

    Delete udana_sutta_func

--- pali_bot.py
+++ pali_bot.py
@@ -83,22 +83,6 @@ def generic_command(message: telebot.types.Message) -> None:
         bot.send_message(message.chat.id, print_text)
 
 
-# TODO Unify
-@bot.message_handler(commands=['udana_sutta'])
-def udana_sutta_func(message):
-    print_text = get_text("udana.txt", 'udana_sutta')
-    limit = 4050
-    if len(print_text) >= limit:
-        com_index = print_text.index("<u>")
-        index = delimiter(print_text, limit)
-        for x in range(len(index) - 1):
-            bot.send_message(message.chat.id, print_text[index[x]:index[x + 1]])
-        bot.send_message(message.chat.id, print_text[index[-1]:com_index])
-        bot.send_message(message.chat.id, print_text[com_index:])
-    else:
-        bot.send_message(message.chat.id, print_text)
-
-
 @bot.message_handler(commands=['about_us'])
 def about_us_func(message):
     bot.send_message(message.chat.id, config.about_text)
